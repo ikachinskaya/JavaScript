86458069227c571d85a885a066a151853c5d3278
ikachinskaya <mkiss09@ukr.net>
2021-07-29 01:00:08 +0300

    Add prototype inheritance

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 //==================================================================
-function Student(firstName, lastName, age, isMale) {
-	this.firstName = firstName;
-	this.lastName = lastName;
-	this.age = age;
-	this.isMale = isMale;
-	this.study = study;
+function Animal() {
+	this.exist = function () {
+		return true;
+	}
 }
 
-function study() {
-	return `Student ${this.firstName} ${this.lastName} is studying`;
+const monkeyPrototype = new Animal();
+
+function Monkey(name, appendages) {
+	this.name = name;
+	this.appendages = appendages;
 }
 
-const student1 = new Student('Test', 'Testovich', 22, true);
-const student2 = new Student('Test1', 'Testovich1', 221, true);
+Monkey.prototype = monkeyPrototype;
 
-console.log(student1);
-console.log(student2);
+const pete = new Monkey('Pete', 5);
+const tom = new Monkey('Tom', 5);
+const jerry = new Monkey('jerry', 5);
+const testasdsadas = new Monkey('testasdsadas', 5);
+const sadsa = new Monkey('asdsad', 5);
+
+// pete.__proto__ = monkeyPrototype;
+// tom.__proto__ = monkeyPrototype;
