b9ab26b20be4cd9ee893ad3325c7651578dde3b4
ikachinskaya <mkiss09@ukr.net>
2021-07-29 01:08:30 +0300

    Add UserProto(), AdminProto()

--- index.js
+++ index.js
@@ -1,24 +1,63 @@
 //==================================================================
-function Animal() {
-	this.exist = function () {
-		return true;
+function UserProto() {
+	this.login = function () {
+	}
+
+	this.logout = function () {
+		return 'you logged out'
+	}
+
+	this.createComment = function () {
 	}
 }
 
-const monkeyPrototype = new Animal();
+function User(username, email, password) {
+	this.username = username;
+	this.email = email;
+	this.password = password;
+	this.isAdmin = false;
+	this.isBanned = false;
+}
+//создаем объект прототип User
+const userPrototype = new UserProto();
 
-function Monkey(name, appendages) {
-	this.name = name;
-	this.appendages = appendages;
+//говорим, что у User прототип - userPrototype
+User.prototype = userPrototype;
+
+const user1 = new User('destrouer997', 'destr@mylo.com', '12343');
+
+function AdminProto() {
+	this.ban = function (user) {
+		if (user.isAdmin === true) {
+			throw new Error('Нельзя банить админов');
+		}
+		user.isBanned = true;
+		return user;
+	}
 }
 
-Monkey.prototype = monkeyPrototype;
+function Admin(username, email, password) {
+	this.username = username;
+	this.email = email;
+	this.password = password;
+	this.isAdmin = true;
+	this.isBanned = false;
+}
 
-const pete = new Monkey('Pete', 5);
-const tom = new Monkey('Tom', 5);
-const jerry = new Monkey('jerry', 5);
-const testasdsadas = new Monkey('testasdsadas', 5);
-const sadsa = new Monkey('asdsad', 5);
+//создаем объект прототип Admin
+const admProto = new AdminProto();
 
-// pete.__proto__ = monkeyPrototype;
-// tom.__proto__ = monkeyPrototype;
+//говорим, что у AdminProto прототип - userPrototype
+AdminProto.prototype = userPrototype;
+
+//говорим, что у Admin прототип - admProto
+Admin.prototype = admProto;
+
+const admin1 = new Admin('Test', 'Testovich', 12323);
+
+admin1.__proto__ = admProto;
+
+const admin2 = new Admin('Test2', 'Testovich2', 12323);
+// const userPrototype = new UserProto();
+
+// User.prototype = userPrototype;
