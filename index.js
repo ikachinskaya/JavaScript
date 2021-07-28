063428e9efe4fdba4903ae6d5e5a320af4a2ac89
ikachinskaya <mkiss09@ukr.net>
2021-07-28 23:45:13 +0300

    Add while

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
 //==================================================================
-const userInput = +prompt("Введите число");
-alert(userInput % 2 === 0 ? "Четное" : "Нечетное");
+let counter = 0;
+while (true) {
+	counter++;
+	console.log(counter);
+
+	if (counter >= 1000) {
+		break;
+	}
+}
+
+let count = 1000;
+while (true) {
+	console.log(count);
+	count--;
+
+	if (count < 0) {
+		break;
+	}
+}
+
+let counter1 = 0;
+while (true) {
+	counter1++;
+	if (counter1 % 2 === 0) {
+		console.log(counter1);
+	} else if (counter1 >= 1000) {
+		break;
+	}
+}
+
+while (true) {
+  if (counter % 2 === 1) {
+    continue;
+  }
+  counter++;
+  console.log(counter);
+  if (counter >= 1000) {
+    break;
+  }
+}
