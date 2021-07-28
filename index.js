67f745d635821863783ec221470c75a3e972e139
ikachinskaya <mkiss09@ukr.net>
2021-07-28 23:31:22 +0300

    Add calculate

--- index.js
+++ index.js
@@ -1,87 +1,56 @@
-// Function expression - функциональное выражение
-// Сумма двух чисел
-const getSumOfTwoNumbers = function (value1, value2) {
-	if (isNaN(value1 - value2)) {
-		alert("Incorrect data");
+//==================================================================
+// Калькулятор
+const firstNumber = +prompt("Введите первое число");
+const secondNumber = +prompt("Введите второе число");
+const operator = +prompt(
+	"Выберите действие:\n1-Сложение\n2-Умножение\n3-Умножение\n4-Деление"
+);
 
-		return null;
-	} else {
-		const result = value1 + value2;
+switch (operator) {
+	case 1:
+		alert(firstNumber + secondNumber);
+		break;
+	case 2:
+		alert(firstNumber - secondNumber);
+		break;
+	case 3:
+		alert(firstNumber * secondNumber);
+		break;
+	case 4:
+		alert(firstNumber / secondNumber);
+		break;
+	default: {
+		alert("Выберите действие");
 	}
 }
 //==================================================================
-
-const myFirstJSFunc = function () {
-	alert("Hello, world!");
-};
-
-myFirstJSFunc();
-//==================================================================
-
-// Найти из двух чисел минимальное
-const getMinNumbers = function (value1, value2) {
-	if (isNaN(value1 - value2)) {
+const firstNumber = +prompt('number 1');
+const secondNumber = +prompt('number 2');
+const operator = prompt('Enter operator');
+const calculate = function (firstNumber, secondNumber, operator) {
+	if (isNaN(firstNumber - secondNumber)) {
 		return null;
 	}
-	if (value1 < value2) {
-		return value1;
+	switch (operator) {
+		case "+": {
+			const result = firstNumber + secondNumber;
+			return result;
+		}
+		case "-": {
+			const result = firstNumber + secondNumber;
+			return result;
+		}
+		case "*": {
+			const result = firstNumber + secondNumber;
+			return result;
+		}
+		case "/": {
+			const result = firstNumber + secondNumber;
+			return result;
+		}
+		default: {
+			return null;
+		}
 	}
-	return value2;
 };
-//==================================================================
-
-// Найти из двух чисел максимальное
-const getMaxNumbers = function (value1, value2) {
-	if (isNaN(value1 - value2)) {
-		return null;
-	}
-	if (value1 > value2) {
-		return value1;
-	}
-	return value2;
-};
-//==================================================================
-
-// Проверить число на четность
-const isEvenNumbers = function (value) {
-	if (value % 2 === 0) {
-		return true;
-	} else return false;
-	return value % 2 === 0; //т.к. value % 2 === 0 - это true!
-};
-//==================================================================
-
-// Решить квадраное уравнение
-// 1 вариант
-const solveSquareExpression = function (a, b, c) {
-	if (isNaN(a - b - c)) {
-		return null;
-	}
-	const d = b * b - 4 * a * c;
-	if (d < 0) {
-		return NaN;
-	}
-	if (d === 0) {
-		const x = (-b / 2) * a;
-		return x;
-	}
-	const dSquareRoot = d ** (1 / 2);
-	const x1 = (-b + dSquareRoot) / (2 * a);
-	const x2 = (-b - dSquareRoot) / (2 * a);
-	return [x1, x2];
-};
-
-// 2 вариант
-const solveSquareExpression = function (a, b, c) {
-	if (isNaN(a - b - c)) {
-		return null;
-	}
-	const d = b * b - 4 * a * c;
-	if (d < 0) {
-		return NaN;
-	}
-	const dSquareRoot = d ** (1 / 2);
-	const x1 = (-b + dSquareRoot) / (2 * a);
-	const x2 = (-b - dSquareRoot) / (2 * a);
-	return [x1, x2];
-};
+calculate();
