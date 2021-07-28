1e15e1a7ad1fefbc5705909a169ec218dcdb27bf
ikachinskaya <mkiss09@ukr.net>
2021-07-29 01:38:38 +0300

    Add filter

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 //==================================================================
-const users = [
-	{ firstName: "User2", lastName: "Test", age: 13 },
-	{ firstName: "User1", lastName: "Test", age: 20 },
-	{ firstName: "User3", lastName: "Test", age: 50 },
-	{ firstName: "User4", lastName: "Test", age: 77 },
-	{ firstName: "User5", lastName: "Test", age: 45 },
-	{ firstName: "User6", lastName: "Test", age: 19 },
-	{ firstName: "User7", lastName: "Test", age: 415 },
-];
+const phones = [
+	{ model: '11', brand: "Apple", price: 150000 },
+	{ brand: "motorolla" },
+	{ brand: "Google" },
+	{ brand: "Meizu" },
+	{ brand: "Xiaobao" },
+	{ model: 'X', brand: "Apple", price: 15000 },
+	{ brand: "Huawei" },
+	{ brand: "Honor" }
+]
 
-function isAdult(user) {
-	return user.age >= 18;
+const checkBrand = function (brand, targetBrand) {
+	return brand === targetBrand;
 }
-// Метод some() проверяет, удовлетворяет ли какой - либо элемент массива условию, заданному в передаваемой функции.
-const someVerdict = users.some(isAdult);
 
-// Метод every() проверяет, удовлетворяют ли все элементы массива условию, заданному в передаваемой функции.
-const everyVerdict = users.every(isAdult);
+const callback = function (phone) {
+	return checkBrand(phone.brand, "Apple");
+}
 
-console.log(someVerdict);
-console.log(everyVerdict);
+const applePhones = phones.filter(callback);
+
+const cheapApplePhones = applePhones.filter(function (phone) {
+	return phone.price <= 20000;
+})
