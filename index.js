c0d2dc905b84f5a06ff05c009512048d4b85450e
ikachinskaya <mkiss09@ukr.net>
2021-07-29 00:39:00 +0300

    Add functions sayHello(user)

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //==================================================================
-const num1 = +prompt("Enter first number");
-const num2 = +prompt("Enter second number");
-alert(`${num1} + ${num2} = ${num1 + num2}`);
-alert(`${num1} - ${num2} = ${num1 - num2}`);
-alert(`${num1} * ${num2} = ${num1 * num2}`);
-alert(`${num1} / ${num2} = ${num1 / num2}`);
+const user = {
+	firstName: 'Null',
+	lastName: 'Nullovna',
+	age: 18,
+	isMale: false
+}
+const user1 = {
+	firstName: 'Undefined',
+	lastName: 'Undefinov',
+	age: 18,
+	isMale: true
+}
+
+function sayHello(user) {
+	return `Hello ${user.firstName} ${user.lastName}!`;
+}
+console.log(sayHello(user));
+console.log(sayHello(user1));
