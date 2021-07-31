81b006c1b4ed0b41ceba07b02e29af60487d7fdb
Inna <ikachinskaya13@gmail.com>
2021-08-01 00:41:52 +0300

    Add HumanProto

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 //==================================================================
 "use strict";
-function sum(a, b) {
-	if (typeof a !== 'number' || typeof b !== 'number' || isNaN(a - b)) {
-		throw new TypeError('Invalid number');
-	}
-	if (a < 0) {
-		throw new RangeError('Число a должно быть положительным');
-	}
-	return a + b;
+function Human(name, lastname) {
+	this.name = name;
+	this.lastname = lastname;
 }
-console.log(sum(1, 'test'));
 
-//Reference Error
-console.log(abracadabra);
-
-//SyntaxError
-//...test1;
+function HumanProto() {
+	this.walk = function () {
+		console.log('I am walking rigt now');
+	}
+}
+Human.prototype = new HumanProto();
+const human1 = new Human('Test', 'Testovich');
+console.log(human1);
