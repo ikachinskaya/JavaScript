11b42990a5b1c735144694992c61f92e270f292a
Inna <ikachinskaya13@gmail.com>
2021-08-01 02:32:57 +0300

    Add class CoolerHuman

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 //==================================================================
 "use strict";
-function Human(name, lastname) {
-	this.name = name;
-	this.lastname = lastname;
-}
-
-function HumanProto() {
-	this.walk = function () {
-		console.log('I am walking rigt now');
+class CoolerHuman {
+	constructor(name, lastname, age) {
+		if (isNaN(age) || typeof age !== 'number') {
+			throw new TypeError("Age must be a number");
+		}
+		if (typeof age < 0) {
+			throw new RangeError('Age must be >0');
+		}
+		//свойства создаваемого экземпляра класса
+		this.name = name;
+		this.lastname = lastname;
+		this.age = age;
+	}
+	//методы у экземпляров класса
+	//создаст метод один раз и закинет в объект каждого экземпляра
+	walk = function () {
+		console.log('I am walking right now');
+	}
+	isAdult = function () {
+		return this.age >= 18;
 	}
 }
-Human.prototype = new HumanProto();
-const human1 = new Human('Test', 'Testovich');
-console.log(human1);
+
+const coolerHuman1 = new CoolerHuman('Coolkid', 'Coolkidovich', 18);
+const coolerHuman2 = new CoolerHuman('Coolkid', 'Coolkidovich', 5);
+
+console.log(coolerHuman1);
+console.log(coolerHuman2);
