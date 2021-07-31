4932d91acf12f7144045ca58228b0562019fe9c1
Inna <ikachinskaya13@gmail.com>
2021-07-31 23:49:21 +0300

    Add rest & spread operator, reduce

--- index.js
+++ index.js
@@ -1,76 +1,44 @@
 //==================================================================
 "use strict";
-/*Функции-стрелки:
-- Только function expression;
-- Не имеет своего this
-- Не работает с arguments*/
+/*...rest - остаточные параметры
+... - собери оставшиеся параметры и положи их в массив
+...rest всегда должен быть последним*/
 
-// function expression
-const anotherSum = function (a, b) {
-	return this;
-};
-
-// function decrlaration
-function sum(a, b) {
-	return this;
-}
-
-const arrowSum = (a, b) => {
-	return a + b;
-};
-
-//если в {} одна строка
-const arrowSum2 = (a, b) => a + b;
-
-//==================================================================
-const newspaper = {
-	title: "NY Post - Murzilka",
-
-	articles: ["Main news", "Article not main", "test1234", "give more money"],
-
-	showArticles() {
-		//const that = this;
-
-		//this: newspaper. Просит this у showArticles, а у него this - newspaper
-		const arrow3 = () => {
-			console.log(this);
-		}
-		arrow3();
-
-		this.articles.forEach((article, index, array) => {
-			console.log(
-				`Newspaper ${this.title} | Article №${index + 1} : ${article}`
-			);
-		});
-	},
-
-	//this:Window, т.к. нет своего this, и он берет выше(т.е. Window)
-	arrow2: () => {
-		console.log(this);
-	}
-};
-
-//this:Window
-const arrow1 = () => {
-	console.log(this);
-}
-//==================================================================
-
-//Четные числа
-const arr = [1, 2, 3, 4];
 //1 вариант
-const evenArr = arr.filter(function (number) {
-	return number % 2 === 0;
-});
-//2 вариант со стрелкой
-const evenArr2 = arr.filter((number) => number % 2 === 0);
-//==================================================================
-
-//Сумма чисел, функция без параметров. Стрелкой не получится.
-const summa = function () {
+const arrowSum = (...rest) => {
 	let result = 0;
-	for (let i = 0; i < arguments.length; i++) {
-		result += arguments[i];
+	for (let i = 0; i < rest.length; i++) {
+		result += rest[i];
 	}
 	return result;
 }
+console.log(arrowSum(5, 6));
+
+//2 вариант
+const arrowSum2 = (...rest) => {
+	const callback = function (accumulator, currentValue) {
+		return accumulator + currentValue;
+	}
+	return rest.reduce(callback);
+}
+console.log(arrowSum2(5, 6));
+
+//со стрелкой
+const arrowSum3 = (...rest) => rest.reduce((accumulator, currentValue) => accumulator + currentValue);
+console.log(arrowSum3(5, 6));
+//==================================================================
+
+/*... - spread-оператор
+- расширяет перебираемый объект в спиоск аргументов
+- работает только с итерируемыми объектами
+- можно использовать для слияния массивов*/
+
+const numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+console.log(...numbers);
+
+const numbers2 = [11, 22, 33, 44, 55, 66];
+console.log(numbers2);
+console.log(...numbers2);
+
+console.log(...numbers, ...numbers2);
