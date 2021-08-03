b341517422b1a9ad6c27837e9f1a7833579f980c
Inna <inna.kachinskaya13@gmail.com>
2021-08-03 22:56:05 +0300

    Add inheritance. Class User, Moderator, Admin

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 //==================================================================
 "use strict";
-class Auto {
-  constructor(weightAuto, maxSpeed, currentSpeed, name) {
-    this.weightAuto = weightAuto;
-    this.maxSpeed = maxSpeed;
-    this.currentSpeed = currentSpeed;
+let thread = [];
+
+class User {
+  constructor(name, lastname, age) {
     this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+    this.isBanned = false;
   }
 
-  set weightAuto(newWeightAuto) {
-    if (isNaN(newWeightAuto) || typeof newWeightAuto !== "number") {
-      throw new TypeError("Invalid type");
-    }
-    if (newWeightAuto < 0) {
-      throw new RangeError("Weigth auto cant be a negative");
-    }
-    this._weightAuto = newWeightAuto;
-  }
-  get weightAuto() {
-    return this._weightAuto;
+  getFullName() {
+    return `${this.name} ${this.lastname}`;
   }
 
-  set maxSpeed(newMaxSpeed) {
-    if (isNaN(newMaxSpeed) || typeof newMaxSpeed !== "number") {
-      throw new TypeError("Invalid type");
-    }
-    if (newMaxSpeed < 0) {
-      throw new RangeError("Max speed auto cant be a negative");
-    }
-    this._maxSpeed = newMaxSpeed;
-  }
-  get maxSpeed() {
-    return this._maxSpeed;
-  }
-
-  set currentSpeed(newCurrentSpeed) {
-    if (isNaN(newCurrentSpeed) || typeof newCurrentSpeed !== "number") {
-      throw new TypeError("Invalid type");
-    }
-    if (newCurrentSpeed < 0) {
-      throw new RangeError("Current speed auto cant be a negative");
-    }
-    this._currentSpeed = newCurrentSpeed;
-  }
-  get currentSpeed() {
-    return this.currentSpeed;
-  }
-
-  set name(newName) {
-    if (typeof newName !== "string") {
-      throw new TypeError("Invalid type");
-    }
-    if (newName.trim() === "") {
-      throw new Error("Name must not be empty");
-    }
-    this._name = newName.trim();
-  }
-  get name() {
-    return this._name;
-  }
-
-  /* Статический метод*/ 
-  static isCar(carInstance) {
-    return carInstance instanceof Car;
+  createMessage(message) {
+    thread.push(message);
+    return message;
   }
 }
-const auto1 = new Auto(2000, 300, 350, "Toyota");
-console.log(auto1);
 
-/*Статический метод вызывается для всего класса в целом, а не для отдельного объекта.
-Ключевое слово this не используется.*/
+class Moderator extends User {
+  constructor(name, lastName, age, permission) {
+    super(name, lastName, age);
+    this.permission = permission;
+  }
+  deleteMessage(messageId) {
+    thread.pop();
+  }
+
+  betterDeleteMessage(messageId) {
+    thread = thread.filter((message, i) => {
+      return messageId !== i;
+    });
+  }
+}
+
+class Admin extends Moderator {
+  constructor(name, lastName, age, permission, email) {
+    super(name, lastName, age, permission);
+    this.email = email;
+  }
+  banUsers(user) {
+    user.isBanned = true;
+  }
+}
+
+const user1 = new User("Test", "Testovich", 50);
+const moderator1 = new Moderator("Petr", "Semenovich", 45, {
+  canDeleteMessages: true,
+});
+const admin1 = new Admin(
+  "Admin",
+  "Adminovich",
+  55,
+  { canDeleteUsers: true, canDeleteMessages: true },
+  "test@mail.com"
+);
+
+admin1.createMessage("Привет");
+admin1.createMessage("Здрасьте");
+admin1.createMessage("Hello");
+admin1.createMessage("Я тут");
+admin1.createMessage("Пока!");
+console.log(thread);
+
+admin1.betterDeleteMessage(1);
+console.log(thread);
+
+admin1.banUsers(user1);
+console.log(user1);
