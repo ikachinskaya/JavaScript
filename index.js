9a73194585bfc7a6aa9363b4383d83e59faf475d
Inna <inna.kachinskaya13@gmail.com>
2021-08-05 23:22:02 +0300

    Add string methods

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
-//==================================================================
+ //==================================================================
 "use strict";
 
-class Figure {
-  constructor(name, secret) {
-    this.name = name;
-  }
+const str = "    test    secret   very secret    ";
+console.log(str);
 
-  getArea() {}
+console.log("charAt");
+const firstLetter = str.charAt(5);
+console.log(firstLetter);
+//=============
+console.log("charCodeAt");
+const firstLetterUniCode = str.charCodeAt(9);
+console.log(firstLetterUniCode);
+//=============
+console.log("includes");
+const hasLetter = str.includes("secr");
+console.log(hasLetter);
+//=============
+console.log("trim");
+const trimmed = str.trim();
+console.log(trimmed);
+//=============
+console.log("makeStringCaps");
+function makeStringCaps(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    result += str[i].toUpperCase();
+  }
+  return result;
+}
+console.log(makeStringCaps(str));
+//=============
+console.log("toRandomCase");
+function toRandomCase(str) {
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    Math.random() > 0.5
+      ? (result += str[i].toUpperCase())
+      : (result += str[i].toLowerCase());
+  }
+  return result;
+}
+console.log(toRandomCase(str));
+//=============
+const str2 = "Nikolay Petrovich Ovechkin";
+
+console.log("replace");
+const newStr = str.replace("secret", "null");
+console.log(newStr);
+//=============
+console.log("split");
+const strArr = str2.split("");
+console.log(strArr);
+//=============
+console.log("join");
+const str2Twin = strArr.join("");
+console.log(str2Twin);
+//=============
+
+const str3 = "test secret very secret";
+
+console.log("toJardenCase");
+//1 вариант
+function toJardenCase(str) {
+  const words = str.split(" ");
+  const wordsWithUpper = words.map((word) => {
+    const firstLetter = word[0].toUpperCase();
+    return firstLetter + word.slice(1);
+  });
+  const result = wordsWithUpper.join(" ");
+  return result;
 }
 
-class Triangle extends Figure {
-  constructor(a, h) {
-    super("Треугольник");
-
-    this.a = a;
-    this.h = h;
-  }
-
-  getArea() {
-    return this.a * this.h * 0.5;
-  }
+//2вариант
+function toJardenCaseTwo(str) {
+  return str
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
 }
-
-class Square extends Figure {
-  constructor(a) {
-    super("Квадрат");
-
-    this.a = a;
-  }
-
-  getArea() {
-    return this.a * this.a;
-  }
-}
-
-function getFigureArea(figure) {
-  if (figure instanceof Figure) {
-    return figure.getArea();
-  }
-  throw new TypeError("Это не фигура");
-}
-
-const triangle = new Triangle(2, 5);
-const square = new Square(5);
-const testFigure = new Figure("Тестовая фигура");
-console.log(triangle);
-console.log(square);
-
-console.log(getFigureArea(triangle));
-console.log(getFigureArea(square));
-console.log(getFigureArea(testFigure));
+console.log(toJardenCase(str3));
+console.log(toJardenCaseTwo(str3));
+//=============
