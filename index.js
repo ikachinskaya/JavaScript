c1aeaeca17985c1956ed1603eeb51cb904e9b1b3
Inna <inna.kachinskaya13@gmail.com>
2021-08-08 23:56:58 +0300

    Add Symbol

--- index.js
+++ index.js
@@ -1,83 +1,40 @@
- //==================================================================
+//==================================================================
 "use strict";
 
-const str = "    test    secret   very secret    ";
-console.log(str);
+/*Символ (Symbol) — это уникальный и неизменяемый тип данных, 
+который может быть использован как идентификатор для свойств объектов.*/
 
-console.log("charAt");
-const firstLetter = str.charAt(5);
-console.log(firstLetter);
-//=============
-console.log("charCodeAt");
-const firstLetterUniCode = str.charCodeAt(9);
-console.log(firstLetterUniCode);
-//=============
-console.log("includes");
-const hasLetter = str.includes("secr");
-console.log(hasLetter);
-//=============
-console.log("trim");
-const trimmed = str.trim();
-console.log(trimmed);
-//=============
-console.log("makeStringCaps");
-function makeStringCaps(str) {
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    result += str[i].toUpperCase();
-  }
-  return result;
-}
-console.log(makeStringCaps(str));
-//=============
-console.log("toRandomCase");
-function toRandomCase(str) {
-  let result = "";
-  for (let i = 0; i < str.length; i++) {
-    Math.random() > 0.5
-      ? (result += str[i].toUpperCase())
-      : (result += str[i].toLowerCase());
-  }
-  return result;
-}
-console.log(toRandomCase(str));
-//=============
-const str2 = "Nikolay Petrovich Ovechkin";
+const sym1 = Symbol();
+const sym2 = Symbol();
+const userFrontId = Symbol("user id for front");
 
-console.log("replace");
-const newStr = str.replace("secret", "null");
-console.log(newStr);
-//=============
-console.log("split");
-const strArr = str2.split("");
-console.log(strArr);
-//=============
-console.log("join");
-const str2Twin = strArr.join("");
-console.log(str2Twin);
-//=============
+const user = {
+  id: 0,
+  name: "Feofan",
+  lastName: "Petrov",
+  [userFrontId]: 500,
+};
 
-const str3 = "test secret very secret";
+console.log(user);
+console.log(user[userFrontId]);
 
-console.log("toJardenCase");
-//1 вариант
-function toJardenCase(str) {
-  const words = str.split(" ");
-  const wordsWithUpper = words.map((word) => {
-    const firstLetter = word[0].toUpperCase();
-    return firstLetter + word.slice(1);
-  });
-  const result = wordsWithUpper.join(" ");
-  return result;
+//getOwnPropertySymbols - возвращает массив всех символьных свойств, найденных на переданном объекте
+const userSymbol = Object.getOwnPropertySymbols(user);
+console.log(userSymbol);
+
+console.log(user[userSymbol[0]]);
+//==================================================================
+
+const numberArray = [59, 94, 9, 41, 2];
+
+//императивный код
+for (let i = 0; i < numberArray.length; i++) {
+  console.log(numberArray[i]);
 }
 
-//2вариант
-function toJardenCaseTwo(str) {
-  return str
-    .split(" ")
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(" ");
+/*Цикл for..of. для его работы нужен Symbol.iterator
+Symbol.iterator - это метод, возвращающий итератор по умолчанию для объекта.*/
+
+for (const number of numberArray) {
+  console.log(number);
 }
-console.log(toJardenCase(str3));
-console.log(toJardenCaseTwo(str3));
-//=============
