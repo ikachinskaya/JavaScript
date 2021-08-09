d2653ebd009bac14e100f62783df976cb31d32e0
Inna <inna.kachinskaya13@gmail.com>
2021-08-10 02:16:18 +0300

    DOM: addEventListener, search elements

--- index.js
+++ index.js
@@ -1,144 +1,53 @@
 //==================================================================
 "use strict";
 
-/*Деструктуризация - особый способ создания переменных*/
+/*DOM - Document Object Model
+  Каждый HTML-тег - объект. 
+  Вложенные теги - дети родительского элемента.
+  Текст внутри тега - объект.
+  DOM - представление HTML-документа в виде дерева тегов.
+  Каждый узел - объект.
+  Теги - узлы-элементы(элементы).
+  Текст внутри элементов - текстовый узел. Содержит только строку, не имеет потомков.
+  Пробелы и переводы троки - текстоые узлы.
+  */
 
-/*Деструктуризация объектов*/
-const monitor = {
-  matrix: "Ips",
-  sizes: {
-    width: {
-      value: 50,
-      unit: "cm",
-    },
-    height: {
-      value: 20,
-      unit: "cm",
-    },
-  },
-  resolution: {
-    horizontal: {
-      value: 1920,
-      unit: "px",
-    },
+//Нашли кнопки
+const [firstBtn, , thirdBtn] = document.getElementsByTagName("button");
 
-    vertical: {
-      value: 1080,
-      unit: "px",
-    },
-  },
-  color: "black",
-  manufactures: "Samsung",
-  refreshRate: 60,
-};
-
-const {
-  matrix: monitorMatrix,
-  sizes: {
-    width: { value: monitorWidth },
-    height: { value: monitorHeight },
-  },
-  resolution: {
-    horizontal: { value: monitorHorizontal },
-    vertical: { value: monitorVertical },
-  },
-
-  color: monitorColor,
-  manufactures: monitorManufactures,
-  refreshRate: monitorRefreshRate,
-} = monitor;
-
-console.log(monitorMatrix);
-console.log(monitorWidth);
-console.log(monitorHeight);
-console.log(monitorHorizontal);
-console.log(monitorVertical);
-console.log(monitorColor);
-console.log(monitorManufactures);
-console.log(monitorRefreshRate);
-console.log("===============");
-
-const {
-  matrix: monitorMatrix_,
-  sizes: {
-    width: { value: monitorWidth_ },
-    height: { value: monitorHeight_ },
-  },
-  resolution: {
-    horizontal: { value: monitorHorizontal_ },
-    vertical: { value: monitorVertical_ },
-  },
-
-  ...restOfMonitor
-} = monitor;
-console.log(restOfMonitor);
-console.log("===============");
-
-//==================================================================
-
-/*Деструктуризация массивов*/
-const array = [-999, 2, 58, 9];
-console.log(array);
-
-const [first] = array;
-console.log(first);
-
-const [one, , three] = array;
-console.log(three);
-
-const [first_, ...restOfArray] = array;
-console.log(restOfArray);
-console.log("===============");
-//==================================================================
-/*Деструктуризация в функциях*/
-
-function calculateDiagonalOfMonitor(monitor) {
-  const {
-    sizes: {
-      width: { value: monitorWidth },
-      height: { value: monitorHeight },
-    },
-  } = monitor;
-
-  return Math.sqrt(monitorWidth ** 2 + monitorHeight ** 2);
+let count = 0;
+//Создали функцию слушателя
+function btnAlert() {
+  alert(`Вы нажали кнопку ${++count}`);
 }
 
-console.log(calculateDiagonalOfMonitor(monitor));
-//==================================================================
-function calculateDiagonalOfMonitor_({
-  sizes: {
-    width: { value: monitorWidth },
-    height: { value: monitorHeight },
-  },
-}) {
-  return Math.sqrt(monitorWidth ** 2 + monitorHeight ** 2);
-}
-
-console.log(calculateDiagonalOfMonitor_(monitor));
-//==================================================================
-const calculateDiagonalOfMonitor__ = ({
-  sizes: {
-    width: { value: monitorWidth },
-    height: { value: monitorHeight },
-  },
-}) => Math.sqrt(monitorWidth ** 2 + monitorHeight ** 2);
-
-console.log(calculateDiagonalOfMonitor__(monitor));
-console.log("===============");
+//Сказали кнопке при нажатии на нее, запускать функцию.
+firstBtn.addEventListener("click", btnAlert);
+thirdBtn.addEventListener("click", btnAlert);
 //==================================================================
 
-const user = {
-  name: "Test",
-  lastName: "Testovich",
-};
+const par = document.getElementById("unique");
+console.log(par);
 
-// function sayHi({ name, lastname }) {
-//   return `${name} ${lastname}`;
-// }
-// console.log(sayHi());
+const testClass = document.getElementsByClassName("test");
+console.log(testClass);
 
-function sayHello(user = { name: "Undefined", lastName: "Nullovich" }) {
-  const { name, lastName } = user;
-  return `Hello ${name} ${lastName}`;
-}
-console.log(sayHello());
+const coolerPar = document.querySelector("#unique>span");
+console.log(coolerPar);
+
+//NodeList
+const coolerPars = document.querySelectorAll("p");
+console.log(coolerPar);
+
+const arr = [...coolerPars];
+console.log(arr);
+console.log("==============");
+//==================================================================
+
+const h1 = document.querySelector(".main>h1");
+const img = document.querySelector(".main>img");
+const span = document.querySelector("span");
+
+console.log(h1);
+console.log(img);
+console.log(span);
