08f29b7cf43fa59332e5806c172779df291eb697
Inna <inna.kachinskaya13@gmail.com>
2021-08-30 22:41:13 +0300

    Add recursion

--- index.js
+++ index.js
@@ -1,100 +1,162 @@
 //==================================================================
 "use strict";
 
-/*  Замыкание - функция, которая запоминает свои внешние переменные и может получить к ним доступ.
-Все функции в JS изначально являются замыканиями. */
+/*Рекурсия - функция, которая вызывает сама себя.*/
 
-//Выведет 10
-const value = 10;
-
-function log() {
-  console.log("log function: ", value);
-}
-
-console.log(log());
-console.log("============");
-//====================
-
-//Выведет 20
-function wrapper() {
-  let value = 20;
-  console.log("log function: ", value);
-}
-console.log(wrapper());
-console.log("============");
-//====================
-
-function wrapper() {
-  let value = 20;
-  console.log("log function: ", value);
-  log(); //Выведет 10
-}
-console.log(log());
-console.log("============");
-//====================
-
-function wrapper() {
-  let value = 20;
-
-  function log() {
-    console.log("log function: ", value);
+let count = 0;
+function test() {
+  console.log(count);
+  count++;
+  if (count < 10) {
+    test();
   }
-  console.log("log function: ", value);
-  log(); //Выведет 20
 }
-console.log(wrapper());
-console.log("============");
+//console.log(test());
 //==================================================================
-
-//грязная функция
-let counter = 0;
-function increment() {
-  counter++;
-  return counter;
+/**
+ *
+ * @param {number} number
+ */
+function countDown(number) {
+  console.log(number);
+  if (number > 0) {
+    countDown(number - 1);
+  }
 }
-console.log(increment());
-console.log("============");
-
-//с помощью замыкания
-function makeCounter() {
-  let counter = 0; //замыкание здесь
-  return function increment() {
-    counter++;
-    return counter;
-  };
-}
-const count = makeCounter();
-console.log(count());
-console.log(count());
-console.log(count());
-console.log(count());
-console.log("============");
-
+//console.log(countDown(5));
 //==================================================================
-
-function makeAdder(value) {
+/**
+ *
+ * @param {number} min
+ * @param {number} max
+ */
+function logRange(min, max) {
+  console.log(min);
+  if (min < max) {
+    logRange(min + 1, max);
+  }
+}
+//console.log(logRange(5, 25));
+//==================================================================
+/**
+ *
+ * @param {number} number
+ * @returns {number} сумма чисел от 1 до max
+ */
+function increment(number) {
+  let result = number;
+  if (number > 0) {
+    result += increment(number - 1);
+  }
+  /*for(let i = 0; i<number; i++) {
+    result+=i;
+  }*/
+  return result;
+}
+//console.log(increment(5));
+//==================================================================
+/* Binary Tree*/
+const tree = {
+  value: 10,
+  left: {
+    value: 20,
+    left: {
+      value: 10,
+    },
+  },
+  right: {
+    value: 30,
+  },
+};
+/**
+ * @param {object} tree
+ * @returns {number} сумма всех параметров value
+ */
+function getTreeValue(tree) {
+  const { value, left, right } = tree;
   let result = value;
-  return function (number) {
-    return (result += number);
-  };
+  //console.log(result)
+  if (left) {
+    result += getTreeValue(left);
+  }
+  if (right) {
+    result += getTreeValue(right);
+  }
+  return result;
 }
-const adder = makeAdder(20);
-console.log(adder(10));
-console.log(adder(100));
-console.log(adder(1000));
-console.log(adder(0));
-console.log(adder(-100));
-console.log("============");
-//====================
-
-const makeAdder_ = (value) => (number) => (value += number);
-
-const adder_ = makeAdder_(20);
-console.log(adder_(10));
-console.log(adder_(100));
-console.log(adder_(1000));
-console.log(adder_(0));
-console.log(adder_(-100));
-//====================
+//console.log(getTreeValue(tree));
+//==================================================================
+//Refactoring
+const tree_ = {
+  value: 10,
+  left: {
+    value: 20,
+    left: {
+      value: 10,
+    },
+  },
+  right: {
+    value: 30,
+  },
+};
+/**
+ * @param {object} tree
+ * @param {number} tree.value значение узла дерева
+ * @param {object} tree.left левый узел дерева
+ * @param {object} tree.right правый узел дерева
+ * @returns {number} сумма всех параметров value
+ */
+function getTreeValue_({ value, left, right }) {
+  let result = value;
+  //console.log(result)
+  if (left) {
+    result += getTreeValue_(left);
+  }
+  if (right) {
+    result += getTreeValue_(right);
+  }
+  return result;
+}
+//console.log(getTreeValue_(tree_));
 //==================================================================
 
+/**
+ *
+ * @param {number} number число
+ * @param {number} pow степень
+ * @returns {number}
+ */
+function power(number, pow) {
+  let result = number;
+
+  if (pow === 0) {
+    return 1;
+  }
+  if (pow < 0) {
+    number = 1 / number;
+    pow *= -1;
+    result *= power(number, pow - 1);
+  }
+  if (pow > 0) {
+    result *= power(number, pow - 1);
+  }
+  return result;
+}
+
+console.log(power(2, 3));
+console.log(power(2, -3));
+console.log(power(2));
+//==================================================================
+//Refactoring
+function power_(number, pow) {
+  let result = number;
+  if (pow === 0) {
+    return 1;
+  }
+  return pow < 0
+    ? (result *= power(1 / number, Math.abs(pow - 1)))
+    : (result *= power(number, pow - 1));
+}
+console.log(power_(2, 3));
+console.log(power_(2, -3));
+console.log(power_(2));
