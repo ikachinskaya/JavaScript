8994f70b3702fc40893e39975950edb0ed0c99c5
Inna <inna.kachinskaya13@gmail.com>
2021-09-03 00:27:11 +0300

    Add algorithms

--- index.js
+++ index.js
@@ -1,162 +1,127 @@
+ 
 //==================================================================
 "use strict";
 
-/*Рекурсия - функция, которая вызывает сама себя.*/
+// Сложность алгоритмов
 
-let count = 0;
-function test() {
-  console.log(count);
-  count++;
-  if (count < 10) {
-    test();
+//Линейная сложность O(n)
+// Линейный поиск
+
+const value = 1207;
+const arr = [1, 5, 6, 91, 18, 9, 2, 8, 2, 7];
+
+const testArr = new Array(1000).fill(null).map((_, i) => i + 1);
+
+function linearSearch(arr, value) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      return true;
+    }
   }
+  return false;
 }
-//console.log(test());
 //==================================================================
-/**
- *
- * @param {number} number
- */
-function countDown(number) {
-  console.log(number);
-  if (number > 0) {
-    countDown(number - 1);
+
+//Квадратичная сложность O(n^2), цикл в цикле
+
+function createMultiplicationTable(number) {
+  const table = [];
+  for (let i = 1; i <= number; i++) {
+    for (let j = 1; j <= number; j++) {
+      table.push(`${i} * ${j} = ${i * j}`);
+    }
   }
+  return table;
 }
-//console.log(countDown(5));
 //==================================================================
-/**
- *
- * @param {number} min
- * @param {number} max
- */
-function logRange(min, max) {
-  console.log(min);
-  if (min < max) {
-    logRange(min + 1, max);
+
+//Логарифмическая сложность O(log(n))
+//Бинарный поиск
+
+function binarySearch(arr, value) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.round((end + start) / 2);
+  while (true) {
+    if (arr[middle] === value) {
+      return true;
+    }
+    if (arr[middle] > value) {
+      end = middle;
+      middle = Math.ceil((end + start) / 2);
+    } else {
+      start = middle;
+      middle = (end + start) / 2;
+    }
+    if (start === end && start === middle) {
+      return false;
+    }
   }
+  //return false;
 }
-//console.log(logRange(5, 25));
+
+// console.log(createMultiplicationTable(1));
+// console.log(createMultiplicationTable(2));
+// console.log(createMultiplicationTable(3));
+// console.log(createMultiplicationTable(250));
+
+console.time("Linear");
+console.log(linearSearch(testArr, value));
+console.timeEnd("Linear");
+
+console.time("Quadro");
+console.log(createMultiplicationTable(1000));
+console.timeEnd("Quadro");
+
+console.time("Binary");
+console.log(binarySearch(testArr, value));
+console.timeEnd("Binary");
 //==================================================================
-/**
- *
- * @param {number} number
- * @returns {number} сумма чисел от 1 до max
- */
-function increment(number) {
-  let result = number;
-  if (number > 0) {
-    result += increment(number - 1);
-  }
-  /*for(let i = 0; i<number; i++) {
-    result+=i;
-  }*/
-  return result;
-}
-//console.log(increment(5));
-//==================================================================
-/* Binary Tree*/
-const tree = {
-  value: 10,
-  left: {
-    value: 20,
-    left: {
-      value: 10,
-    },
-  },
-  right: {
-    value: 30,
-  },
+
+//Константная сложность О(1)
+//Односвязный список
+
+let listItem1 = {
+  value: 1,
+  next: listItem2,
 };
-/**
- * @param {object} tree
- * @returns {number} сумма всех параметров value
- */
-function getTreeValue(tree) {
-  const { value, left, right } = tree;
-  let result = value;
-  //console.log(result)
-  if (left) {
-    result += getTreeValue(left);
-  }
-  if (right) {
-    result += getTreeValue(right);
-  }
-  return result;
-}
-//console.log(getTreeValue(tree));
-//==================================================================
-//Refactoring
-const tree_ = {
-  value: 10,
-  left: {
-    value: 20,
-    left: {
-      value: 10,
-    },
-  },
-  right: {
-    value: 30,
-  },
+let listItem2 = {
+  value: 2,
+  next: listItem3,
 };
-/**
- * @param {object} tree
- * @param {number} tree.value значение узла дерева
- * @param {object} tree.left левый узел дерева
- * @param {object} tree.right правый узел дерева
- * @returns {number} сумма всех параметров value
- */
-function getTreeValue_({ value, left, right }) {
-  let result = value;
-  //console.log(result)
-  if (left) {
-    result += getTreeValue_(left);
-  }
-  if (right) {
-    result += getTreeValue_(right);
-  }
-  return result;
-}
-//console.log(getTreeValue_(tree_));
-//==================================================================
+let listItem3 = {
+  value: 3,
+  next: null,
+};
 
-/**
- *
- * @param {number} number число
- * @param {number} pow степень
- * @returns {number}
- */
-function power(number, pow) {
-  let result = number;
+//Индексация и поиск O(n)
+for (let i = 0; i < 2; i++) {}
 
-  if (pow === 0) {
-    return 1;
-  }
-  if (pow < 0) {
-    number = 1 / number;
-    pow *= -1;
-    result *= power(number, pow - 1);
-  }
-  if (pow > 0) {
-    result *= power(number, pow - 1);
-  }
-  return result;
-}
+//Вставка и удаление O(1)
 
-console.log(power(2, 3));
-console.log(power(2, -3));
-console.log(power(2));
-//==================================================================
-//Refactoring
-function power_(number, pow) {
-  let result = number;
-  if (pow === 0) {
-    return 1;
-  }
-  return pow < 0
-    ? (result *= power(1 / number, Math.abs(pow - 1)))
-    : (result *= power(number, pow - 1));
-}
-console.log(power_(2, 3));
-console.log(power_(2, -3));
-console.log(power_(2));
+//Удаление
+let listItem1 = {
+  value: 1,
+  next: null,
+};
+//Вставка
+let listItem1 = {
+  value: 3,
+  next: listItem5,
+};
+
+//Двусвязный список
+let listItem1 = {
+  value: 1,
+  next: listItem2,
+  prev: null,
+};
+let listItem2 = {
+  value: 2,
+  next: listItem3,
+  prev: listItem1,
+};
+let listItem3 = {
+  value: 3,
+  next: listItem2,
+};
