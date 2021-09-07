60ae10947dfd0040daab972c59020996bf2372bb
Inna <inna.kachinskaya13@gmail.com>
2021-09-08 00:24:36 +0300

    JSON

--- index.js
+++ index.js
@@ -1,147 +1,57 @@
 //==================================================================
 "use strict";
 
-/* Структура данных — это конкретный способ хранения и организации данных.  */
+/* 
+Сериализация объектов - это процесс преобразования объектов в строковую форму представления, 
+которая позднее может использоваться для их восстановления.
 
-/* Map
-Map - структура данных, которая хранит данные в парах ключ / значение, где каждый ключ уникален. 
-Запоминает исходный порядок вставки ключей. 
-Любое значение (как объекты, так и примитивные значения) может использоваться как ключ или значение.
-Map иногда называется ассоциативным массивом или словарем. 
-Часто используется для быстрого поиска данных. 
+JSON (JavaScript Object Notation) – это общий формат для представления значений и объектов.
+=========================
+JSON.stringify для преобразования объектов в JSON.
+JSON.stringify(student) берёт объект и преобразует его в строку.
+Полученная строка json называется JSON-форматированным или сериализованным объектом. 
+Его можно отправить по сети или поместить в обычное хранилище данных.
+Отличия от литерала:
+- Строки используют двойные кавычки.
+- Имена свойств объекта также заключаются в двойные кавычки. 
+=========================
+JSON.stringify может быть применён и к примитивам.
 
-new Map() – создаёт коллекцию.
-map.set(key, value) – записывает по ключу key значение value.
-map.get(key) – возвращает значение по ключу или undefined, если ключ key отсутствует.
-map.has(key) – возвращает true, если ключ key присутствует в коллекции, иначе false.
-map.delete(key) – удаляет элемент по ключу key.
-map.clear() – очищает коллекцию от всех элементов.
-map.size – возвращает текущее количество элементов.
+JSON поддерживает следующие типы данных:
 
-Перебор Map
-map.keys() – возвращает итерируемый объект по ключам,
-map.values() – возвращает итерируемый объект по значениям,
-map.entries() – возвращает итерируемый объект по парам вида [ключ, значение], этот вариант используется по умолчанию в for..of.
+Объекты { ... }
+Массивы [ ... ]
+Примитивы:
+строки,
+числа,
+логические значения true/false,
+null.
+=========================
+JSON.stringify пропускает некоторые специфические свойства объектов JavaScript:
+Свойства-функции (методы).
+Символьные свойства.
+Свойства, содержащие undefined.
+=========================
+JSON не поддерживает комментарии. Добавление комментария в JSON делает его недействительным.
+=========================
+JSON.parse для преобразования JSON обратно в объект.
 */
-const user = {
-  id: 2,
-};
 
 const obj = {
   id: 1,
-  name: "Test",
-  lastName: "Test",
-  id: 1000,
-  age: 5,
-  0: false,
-  [user]: "Hello",
+  name: 'test',
+  lastName: 'test',
+  age: 100,
+  location: undefined,
+  test() {
+  },
+  [Symbol()]: 'test'
 };
 
-//кортеж - массив, состоящий из ключа и значения
+//сериализация объекта
+const jsonifiedObj = JSON.stringify(obj);
+console.log(jsonifiedObj);
 
-//создает двумерный массив на основании объекта
-//двумерный массив ключей и значений
-const objEntries = Object.entries(obj);
-console.log(objEntries);
-
-//двумерный массив значений
-const objValues = Object.values(obj);
-console.log(objValues);
-
-//двумерный массив ключей
-const objKeys = Object.keys(obj);
-console.log(objKeys);
-
-//длина массива
-const objSize = objEntries.length;
-console.log(objSize);
-
-const map = new Map();
-
-const arr = [1, 34, 5];
-
-const func = () => {};
-
-map.set("id", 1);
-map.set(arr, "array");
-map.set(func, "function");
-map.set(1, 1);
-map.set("1", "number one");
-map.set(user, "object");
-console.log(map);
-
-const value = map.get(user);
-console.log(value);
-map.delete(user);
-console.log(map);
-//==================================================================
-
-//Словарь
-const vocabulary = new Map([
-  ["получить", "obtain"],
-  ["преимущество", "advantage"],
-  ["стул", "chair"],
-  ["наличка", "cash"],
-  ["цифробуквенный", "alphanumerical"],
-  ["переговоры", "negotiacions"],
-  ["значительный", "significant"],
-  ["кот", "cat"],
-  ["собака", "dog"],
-  ["слон", "elephant"],
-  ["язык", "language"],
-]);
-
-const stringToTranslate =
-  "слон значительный получить наличку собака кот цифробуквенный стул";
-
-function translate(str) {
-  const arrFromStr = str.split(" ");
-  for (let i = 0; i < arrFromStr.length; i++) {
-    arrFromStr[i] = vocabulary.get(arrFromStr[i]);
-  }
-  arrFromStr.join(" ");
-  return arrFromStr;
-}
-console.log(translate(stringToTranslate));
-
-//refactoring
-function translate_(str) {
-  return str
-    .split(" ")
-    .map((element) =>
-      vocabulary.has(element) ? vocabulary.get(element) : element
-    )
-    .join(" ");
-}
-console.log(translate_(stringToTranslate));
-//==================================================================
-const u1 = {
-  id: 1,
-  name: "John",
-  age: 20,
-};
-const u2 = {
-  id: 2,
-  name: "Jane",
-  age: 23,
-};
-const johnMsg = ["Hello", "yes", "no", "test"];
-const janeMsg = ["Hi", "abracadabra?", "test", "not test"];
-
-const msgToUser = new Map();
-
-msgToUser.set(u1, johnMsg);
-msgToUser.set(u2, janeMsg);
-
-function getUserMessege(user) {
-  return msgToUser.get(user);
-}
-console.log(getUserMessege(u1));
-
-msgToUser.set(u1.id, johnMsg);
-msgToUser.set(u2.id, janeMsg);
-
-function getUserMessege_({ id }) {
-  return msgToUser.get(id);
-}
-console.log(getUserMessege_({id:2}));
+//десериализация
+const rebuildedObj = JSON.parse(jsonifiedObj); 
+console.log(rebuildedObj);
