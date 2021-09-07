e1cf9429be0433bb569c473d65777ab9c9d5c5f5
Inna <inna.kachinskaya13@gmail.com>
2021-09-07 23:56:29 +0300

    Add setInterval, setTimeOut

--- index.js
+++ index.js
@@ -1,147 +1,73 @@
 //==================================================================
 "use strict";
 
-/* Структура данных — это конкретный способ хранения и организации данных.  */
+/* 
+setTimeout позволяет вызвать функцию один раз через определённый интервал времени.
+setInterval позволяет вызывать функцию регулярно, повторяя вызов через определённый интервал времени.
+timerId - идентификатор таймера
+clearTimeout(timerId), clearInterval(timerId) - отмена дальнейшего выполнения
 
-/* Map
-Map - структура данных, которая хранит данные в парах ключ / значение, где каждый ключ уникален. 
-Запоминает исходный порядок вставки ключей. 
-Любое значение (как объекты, так и примитивные значения) может использоваться как ключ или значение.
-Map иногда называется ассоциативным массивом или словарем. 
-Часто используется для быстрого поиска данных. 
 
-new Map() – создаёт коллекцию.
-map.set(key, value) – записывает по ключу key значение value.
-map.get(key) – возвращает значение по ключу или undefined, если ключ key отсутствует.
-map.has(key) – возвращает true, если ключ key присутствует в коллекции, иначе false.
-map.delete(key) – удаляет элемент по ключу key.
-map.clear() – очищает коллекцию от всех элементов.
-map.size – возвращает текущее количество элементов.
-
-Перебор Map
-map.keys() – возвращает итерируемый объект по ключам,
-map.values() – возвращает итерируемый объект по значениям,
-map.entries() – возвращает итерируемый объект по парам вида [ключ, значение], этот вариант используется по умолчанию в for..of.
 */
-const user = {
-  id: 2,
-};
 
-const obj = {
-  id: 1,
-  name: "Test",
-  lastName: "Test",
-  id: 1000,
-  age: 5,
-  0: false,
-  [user]: "Hello",
-};
+// const btn = document.querySelector("#btn");
 
-//кортеж - массив, состоящий из ключа и значения
+// btn.addEventListener("click", (e) => {
+//   console.log("btn");
+// });
 
-//создает двумерный массив на основании объекта
-//двумерный массив ключей и значений
-const objEntries = Object.entries(obj);
-console.log(objEntries);
+// console.log("Hi");
 
-//двумерный массив значений
-const objValues = Object.values(obj);
-console.log(objValues);
+// const id = setTimeout(function callback1() {
+//   console.log("Hello World");
+// }, 500);
 
-//двумерный массив ключей
-const objKeys = Object.keys(obj);
-console.log(objKeys);
+// clearTimeout(id);
+// //==================================================================
+// let i = 0;
+// const intervalId = setInterval(() => {
+//   console.log(`interval${i++}`);
+// }, 500);
+// console.log(intervalId);
 
-//длина массива
-const objSize = objEntries.length;
-console.log(objSize);
-
-const map = new Map();
-
-const arr = [1, 34, 5];
-
-const func = () => {};
-
-map.set("id", 1);
-map.set(arr, "array");
-map.set(func, "function");
-map.set(1, 1);
-map.set("1", "number one");
-map.set(user, "object");
-console.log(map);
-
-const value = map.get(user);
-console.log(value);
-map.delete(user);
-console.log(map);
+// clearInterval(intervalId);
 //==================================================================
 
-//Словарь
-const vocabulary = new Map([
-  ["получить", "obtain"],
-  ["преимущество", "advantage"],
-  ["стул", "chair"],
-  ["наличка", "cash"],
-  ["цифробуквенный", "alphanumerical"],
-  ["переговоры", "negotiacions"],
-  ["значительный", "significant"],
-  ["кот", "cat"],
-  ["собака", "dog"],
-  ["слон", "elephant"],
-  ["язык", "language"],
-]);
+/*
+Создать функции, которые последовательно выводят в консоль числа от 1 до 20 с интервалом в 100 мс.
+Можно решить через 
+- setInterval - if, clearInterval
+- setTimeout - рекурсия с if
+Посчитать время выполнения:
+- console.time() 
+- console.timeEnd()
+*/
 
-const stringToTranslate =
-  "слон значительный получить наличку собака кот цифробуквенный стул";
-
-function translate(str) {
-  const arrFromStr = str.split(" ");
-  for (let i = 0; i < arrFromStr.length; i++) {
-    arrFromStr[i] = vocabulary.get(arrFromStr[i]);
-  }
-  arrFromStr.join(" ");
-  return arrFromStr;
+function countInterval() {
+  let count = 1;
+  const idInterval = setInterval(() => {
+    console.log(count++);
+    if (count > 20) {
+      clearInterval(idInterval);
+    }
+  }, 100);
 }
-console.log(translate(stringToTranslate));
 
-//refactoring
-function translate_(str) {
-  return str
-    .split(" ")
-    .map((element) =>
-      vocabulary.has(element) ? vocabulary.get(element) : element
-    )
-    .join(" ");
-}
-console.log(translate_(stringToTranslate));
+console.time("SetInterval");
+console.log(countInterval());
+console.timeEnd("SetInterval");
 //==================================================================
-const u1 = {
-  id: 1,
-  name: "John",
-  age: 20,
-};
-const u2 = {
-  id: 2,
-  name: "Jane",
-  age: 23,
-};
-const johnMsg = ["Hello", "yes", "no", "test"];
-const janeMsg = ["Hi", "abracadabra?", "test", "not test"];
 
-const msgToUser = new Map();
+function countTimeOut(count) {
+  let timeId = setTimeout(() => {
+    if (count <= 20) {
+      console.log(count++);
+      timeId = countTimeOut(count);
+    }
+  }, 100);
 
-msgToUser.set(u1, johnMsg);
-msgToUser.set(u2, janeMsg);
-
-function getUserMessege(user) {
-  return msgToUser.get(user);
 }
-console.log(getUserMessege(u1));
 
-msgToUser.set(u1.id, johnMsg);
-msgToUser.set(u2.id, janeMsg);
-
-function getUserMessege_({ id }) {
-  return msgToUser.get(id);
-}
-console.log(getUserMessege_({id:2}));
+console.time("SetTimeOut");
+console.log(countTimeOut(1));
+console.timeEnd("SetTimeOut");
